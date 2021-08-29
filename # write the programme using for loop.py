839baa32678669c4fcf800f198280b1fffea74aa
aniket889 <89179292+aniket889@users.noreply.github.com>
2021-08-29 17:50:17 +0530

    Add files via upload

---
+++ # write the programme using for loop.py
@@ -0,0 +1,5 @@
+# write the programme using for loop
+
+n=int(input("enter the number of rows"))
+for i in range(1,n+1):
+    print(i," "*n)
---
+++ # write the programme using the loop.py
@@ -0,0 +1,5 @@
+# write the programme using the loop
+
+n=int (input("enter the number of rows:"))
+for i in range(1,n+1):
+    print(i," "*n)
